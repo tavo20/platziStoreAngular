fb90ad1bcb9f532a9e1863f915be0f8309cfaa52
Imagine Apps <gustavo.jimenez@imagineapps.co>
2020-02-12 07:34:28 -0500

    Implementacion de google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
-import {Product} from './product.model';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(
+    private router: Router
+  ) {
+  const navEndEvents$ = this.router.events
+      .pipe(
+        filter( event => event instanceof NavigationEnd)
+      );
+    navEndEvents$.subscribe( (event: NavigationEnd) => {
+      gtag('config', 'UA-158282429-1', {
+        page_path: event.urlAfterRedirects
+      });
+    })
+
+  }
 }
